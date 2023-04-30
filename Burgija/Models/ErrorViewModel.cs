4bf05d66be60afab61c862945e356339dab4382d
zlatanjakic <zlatan.jakic@klika.ba>
2023-04-30 17:22:57 +0200

    Added the project folder

---
+++ Burgija/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Burgija.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
