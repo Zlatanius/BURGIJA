456c1ea416bd211c5b67fd20c924301107dee70b
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-05-27 12:09:55 +0200

    added authoriaztion for admin

--- Burgija/Controllers/AdminPanelController.cs
+++ Burgija/Controllers/AdminPanelController.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Burgija.Data;
 using Burgija.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Burgija.Controllers
 {
+    [Authorize(Roles = "Administrator")]
     public class AdminPanelController : Controller
     {
         private readonly ApplicationDbContext _context;
--- Burgija/Startup.cs
+++ Burgija/Startup.cs
@@ -33,6 +33,7 @@ namespace Burgija
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
         }
