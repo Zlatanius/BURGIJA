6e23a6fba21a9c5e804e6f3d38a5d187c5c4b95a
jasarevicedis <e.jasarevic03>
2023-06-05 13:14:19 +0200

    removed image and price from tool, added them to tooltype, migrated to db

--- Burgija/Models/ToolType.cs
+++ Burgija/Models/ToolType.cs
@@ -15,16 +15,21 @@ namespace Burgija.Models
         public Category Category { get; set; }
         public string Description { get; set; }
 
+        public double Price { get; set; }
+        public string Image { get; set; }
+
         #endregion
 
         #region Constructors
 
-        public ToolType(int id, string name, Category category, string description) 
+        public ToolType(int id, string name, Category category, string description, double price, string image) 
         {
             Id = id;
             Name = name;
             Category = category;
             Description = description;
+            Price = price;
+            Image = image;
         }
 
         public ToolType() { }
