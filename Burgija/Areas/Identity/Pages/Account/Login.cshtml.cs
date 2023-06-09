be57c6e8f707eb4505bfbc309b5826213628040e
aljubuncic <aljubuncic1@etf.unsa.ba>
2023-06-09 10:56:42 +0200

    resolved login and register

--- Burgija/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Burgija/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -81,7 +81,7 @@ namespace Burgija.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Username, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.Username, Input.Password,false, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
