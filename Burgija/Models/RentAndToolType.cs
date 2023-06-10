a9a9def91e1596d4fbdc2763dce2b2c2a9a919fe
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-06-10 07:30:12 -0700

    added elements shownig in rent history list

---
+++ Burgija/Models/RentAndToolType.cs
@@ -0,0 +1,13 @@
+﻿namespace Burgija.Models
+{
+    public class RentAndToolType
+    {
+        public Rent Rent { get; set; }
+        public ToolType ToolType { get; set; }
+        public RentAndToolType(Rent rent, ToolType toolType)
+        {
+            Rent = rent;
+            ToolType = toolType;
+        }
+    }
+}
