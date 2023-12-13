5a49968356ab600de16236b3643e461caa52c4a0
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-12-13 12:28:10 +0100

    resolved issue with documentation

--- Burgija/Controllers/AdminPanelController.cs
+++ Burgija/Controllers/AdminPanelController.cs
@@ -11,18 +11,26 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace Burgija.Controllers
 {
+    // Authorize only users with the "Administrator" role to access this controller
     [Authorize(Roles = "Administrator")]
     public class AdminPanelController : Controller
     {
+        // Database context for interacting with the underlying data store
         private readonly ApplicationDbContext _context;
+
+        // Lists to store information about stores and tool types
         private List<Store> stores;
         private List<ToolType> toolTypes;
 
+        // Constructor that takes the application database context as a dependency
         public AdminPanelController(ApplicationDbContext context)
         {
             _context = context;
         }
 
+        /// <summary>
+        /// Helper method to initialize lists of stores and tool types asynchronously.
+        /// </summary>
         private async Task InitializeLists()
         {
             stores = await _context.Store.ToListAsync();
@@ -34,27 +42,29 @@ namespace Burgija.Controllers
             toolTypes = await _context.ToolType.ToListAsync();
         }
 
-        // GET: AdminPanel
+        /// <summary>
+        /// Displays a list of tools in the admin panel.
+        /// </summary>
         public async Task<IActionResult> Index()
         {
             await InitializeLists();
             var tools = await _context.Tool.ToListAsync();
-            foreach(var tool in tools)
+            foreach (var tool in tools)
             {
                 tool.Store = stores.Find(store => store.Id == tool.StoreId);
             }
-
             return View(tools);
         }
 
-        // GET: AdminPanel/Details/5
+        /// <summary>
+        /// Displays details of a specific tool in the admin panel.
+        /// </summary>
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
-
             var tool = await _context.Tool
                 .Include(t => t.Store)
                 .Include(t => t.ToolType)
@@ -69,7 +79,9 @@ namespace Burgija.Controllers
             return View(tool);
         }
 
-        // GET: AdminPanel/AddTool
+        /// <summary>
+        /// Displays a form for adding a new tool in the admin panel.
+        /// </summary>
         public async Task<IActionResult> AddTool()
         {
             await InitializeLists();
@@ -78,11 +90,9 @@ namespace Burgija.Controllers
             return View();
         }
 
-        
-
-        // POST: AdminPanel/AddTool
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// Handles the submission of the form for adding a new tool.
+        /// </summary>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> AddTool([Bind("Id,ToolTypeId,StoreId,Price,Image")] Tool tool)
@@ -98,14 +108,15 @@ namespace Burgija.Controllers
             return View(tool);
         }
 
-        // GET: AdminPanel/Edit/5
+        /// <summary>
+        /// Displays a form for editing a specific tool in the admin panel.
+        /// </summary>
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
-
             var tool = await _context.Tool.FindAsync(id);
             if (tool == null)
             {
@@ -117,9 +128,9 @@ namespace Burgija.Controllers
             return View(tool);
         }
 
-        // POST: AdminPanel/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// Handles the submission of the form for editing a specific tool.
+        /// </summary>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,ToolTypeId,StoreId,Price,Image")] Tool tool)
@@ -154,14 +165,15 @@ namespace Burgija.Controllers
             return View(tool);
         }
 
-        // GET: AdminPanel/Remove/5
+        /// <summary>
+        /// Displays details of a specific tool for removal in the admin panel.
+        /// </summary>
         public async Task<IActionResult> Remove(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
-
             var tool = await _context.Tool
                 .Include(t => t.Store)
                 .Include(t => t.ToolType)
@@ -176,7 +188,9 @@ namespace Burgija.Controllers
             return View(tool);
         }
 
-        // POST: AdminPanel/Remove/5
+        /// <summary>
+        /// Handles the confirmation of removing a specific tool.
+        /// </summary>
         [HttpPost, ActionName("Remove")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> RemoveConfirmed(int id)
@@ -187,9 +201,14 @@ namespace Burgija.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        /// <summary>
+        /// Checks if a tool with the specified ID exists.
+        /// </summary>
         private bool ToolExists(int id)
         {
             return _context.Tool.Any(e => e.Id == id);
         }
     }
+
+
 }
