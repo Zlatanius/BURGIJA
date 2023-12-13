00cf9f106d6b9ef4ce7961195c12d7b323aa5e0f
Your Name <you@example.com>
2023-12-13 13:22:43 +0100

    added documentation and solved mistakenly deleted implemetation of methods

--- Burgija/Controllers/RentController.cs
+++ Burgija/Controllers/RentController.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Burgija.Data;
 using Burgija.Models;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-// ... (other namespaces)
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using System.Collections;
+using Microsoft.AspNetCore.Http;
+using Burgija.ViewModels;
 
 namespace Burgija.Controllers
 {
@@ -37,7 +42,17 @@ namespace Burgija.Controllers
         [Authorize]
         public async Task<IActionResult> RentHistory()
         {
-            // Implementation...
+            // Retrieve the user ID from claims
+            var userId = Int32.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+
+            // Retrieve the rental history and associated tool information
+            var rentHistory = await _context.Rent
+                .Where(r => r.UserId == userId)
+                .Join(_context.Tool, rent => rent.ToolId, tool => tool.Id, (rent, tool) => new { Rent = rent, Tool = tool })
+                .Join(_context.ToolType, rt => rt.Tool.ToolTypeId, toolType => toolType.Id, (rt, toolType) => new RentAndToolType(rt.Rent, toolType))
+                .ToListAsync();
+
+            return View(rentHistory);
         }
 
         /// <summary>
@@ -48,7 +63,17 @@ namespace Burgija.Controllers
         [HttpPost]
         public IActionResult GetToolType(int? toolTypeId)
         {
-            // Implementation...
+  
+            if (toolTypeId == null)
+            {
+                return NotFound();
+            }
+
+     
+            HttpContext.Session.SetInt32("ToolType", (int)toolTypeId);
+
+            
+            return RedirectToAction("Create", new { toolTypeId });
         }
 
         /// <summary>
@@ -58,7 +83,33 @@ namespace Burgija.Controllers
         /// <returns>The rental creation view.</returns>
         public async Task<IActionResult> Create(int? toolTypeId)
         {
-            // Implementation...
+            
+            if (toolTypeId == null)
+            {
+                return NotFound();
+            }
+
+            
+            var toolType = await _context.ToolType.FirstOrDefaultAsync(m => m.Id == toolTypeId);
+
+            
+            if (toolType == null)
+            {
+                return NotFound();
+            }
+
+            
+            ViewBag.ToolTypeImage = toolType.Image;
+
+            
+            ViewData["DiscountId"] = new SelectList(_context.Discount, "Id", "Id");
+            ViewData["ToolId"] = new SelectList(_context.Tool, "Id", "Id");
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+
+            
+            ViewBag.ToolType = toolType;
+
+            return View();
         }
 
         /// <summary>
@@ -70,7 +121,56 @@ namespace Burgija.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("StartOfRent,EndOfRent,DiscountId")] Rent r)
         {
-            // Implementation...
+            
+            if (r.EndOfRent < r.StartOfRent)
+                return BadRequest("Date of return is earlier than date of taking");
+
+            
+            if (r.StartOfRent < DateTime.Now || r.EndOfRent < DateTime.Now)
+                return BadRequest("Date of taking or date of return is earlier than today");
+            
+            r.UserId = Int32.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+
+            
+            var toolTypeId = HttpContext.Session.GetInt32("ToolType");
+
+            
+            var toolIds = await _context.Tool
+                .Where(tool => tool.ToolType.Id == toolTypeId)
+                .Where(tool => !_context.Rent.Any(rent =>
+                    rent.ToolId == tool.Id &&
+                    (r.EndOfRent < rent.StartOfRent || r.StartOfRent > rent.EndOfRent)))
+                .Select(tool => tool.Id)
+                .ToListAsync();
+
+            
+            if (toolIds.Count > 0)
+                r.ToolId = toolIds[0];
+            else
+                return BadRequest("There are no tools available in this period");
+
+            
+            r.DiscountId = null;
+            
+            var toolType = await _context.ToolType.FindAsync(toolTypeId);
+            r.RentPrice = toolType.Price * r.EndOfRent.Subtract(r.StartOfRent).TotalDays;
+
+            
+            if (ModelState.IsValid)
+            {
+            
+                _context.Add(r);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("RentHistory");
+            }
+            
+            ViewData["DiscountId"] = new SelectList(_context.Discount, "Id", "Id", r.DiscountId);
+            ViewData["ToolId"] = new SelectList(_context.Tool, "Id", "Id", r.ToolId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", r.UserId);
+
+            return RedirectToAction("Create", new { toolTypeId });
         }
     }
 }
+
